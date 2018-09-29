94d009a8dea2ff44d7676ab91ad697fd377b9baa
Ryan Ray <rr3149@nyu.edu>
2018-09-29 15:13:13 -0400

    Fixed Destroying objects and scripts

--- Assets/Scripts/Destroying.cs
+++ Assets/Scripts/Destroying.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Destroying : MonoBehaviour
 {
 
-	public GameObject peoplePrefab;
+	public GameObject playerObj;    //name player object
+	Vector3 cameraOffSet;   //offsets the camera
+
+	public float x, y, z;
+
+	// Use this for initialization
+	void Start () {
+
+		playerObj = GameObject.Find("Player");  //locate player
+		cameraOffSet = new Vector3(x, y, z);  //offset camera for 3rd person view
+
+	}
 	
+	// Update is called once per frame
+	void Update () {
 
+		transform.position = playerObj.transform.position + cameraOffSet; //calculate position to fix camera with each update
 
+	}
+	
 	private void OnTriggerEnter(Collider collision)
 	{
-		Destroy(peoplePrefab);
+		if(collision.gameObject.tag == "NPC")
+		{
+			Destroy(collision.gameObject);
+		}
 	}
 }

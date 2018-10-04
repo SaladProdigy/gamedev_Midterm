efe94fd18c7c96e668e0b3f215dd424d9e73390b
Ryan Ray <rr3149@nyu.edu>
2018-10-04 14:44:01 -0400

    Working on a better spawning mechanic

--- Assets/Scripts/Spawning.cs
+++ Assets/Scripts/Spawning.cs
@@ -9,21 +9,27 @@ public class Spawning : MonoBehaviour
 	float timeUntilSpawn;
 	float spawnDelay;
 	public GameObject player;
-	
-	// Update is called once per frame
-	void Update () {
-		
-		/*timeUntilSpawn -= Time.deltaTime;
-		if (timeUntilSpawn <= 0f)
+
+	private void OnTriggerStay(Collider collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
 		{
+			timeUntilSpawn -= Time.deltaTime;
 
-			//instantiate a new block
-			GameObject newBlock = Instantiate(peoplePrefab, new Vector3(player.transform.position.x, player.transform.position.y, 0f), Quaternion.identity) as GameObject;
-			//newBlock.transform.parent = transform;
-			spawnDelay = .05f;
-			timeUntilSpawn = Random.Range(-0.1f, 0.1f) + spawnDelay;//slightly randomize the delay until the next block
+			if (timeUntilSpawn <= 0f)
+			{
 
-		}*/
-		
+				//instantiate a new block
+				GameObject newBlock = Instantiate(peoplePrefab,
+					new Vector3(player.transform.position.x - 7f, player.transform.position.y, 0.5f),
+					Quaternion.identity) as GameObject;
+				//newBlock.transform.parent = transform;
+				spawnDelay = 0.1f;
+				timeUntilSpawn =
+					Random.Range(-0.1f, 0.1f) + spawnDelay; //slightly randomize the delay until the next block
+
+			}
+		}
 	}
+	
 }
